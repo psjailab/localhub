f55c9c3e4da96e616d3da2a3b301fa167f6a50dd
hoyeon <hoyeonzigy@gmail.com>
2026-07-16 11:57:00 +0900

    지도 마커 설정 변경

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -65,6 +65,16 @@
 import { computed, onMounted, onBeforeUnmount, ref, watch } from 'vue'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
+import iconUrl from 'leaflet/dist/images/marker-icon.png'
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png'
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
+
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl
+})
 
 import PlaceCard from '../components/PlaceCard.vue'
 import PlaceListView from '../components/PlaceListView.vue'
